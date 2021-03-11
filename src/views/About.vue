f3c2156a29f7a9126fab2600e5a50fce8f02dba7
Tiny-Zhang <zh_jin123@163.com>
2021-03-11 14:42:33 +0800

    迁入

---
+++ src/views/About.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="about">
+    <div @click="GetUserInfo">点我</div>
+    <div>{{UserData.Username}}</div>
+    <div>{{UserData.TrueName}}</div>
+    <div>{{UserData.Mobile}}</div>
+    <div>{{UserData.Status}}</div>
+    <div>{{UserData.CreatTime}}</div>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      UserData: [],
+      apiUrl: "http://localhost:5000",
+    };
+  },
+  created() {
+    this.GetUserInfo();
+  },
+  methods: {
+    GetUserInfo() {
+      var data = { Username: "032-03-25" };
+      axios
+        .post(`${this.apiUrl}/api/User/GetUserInfo`, data)
+        .then((res) => {
+          //console.log("res=>",res)
+          this.UserData =res.data;
+          console.log(this.UserData)
+        })
+        .catch(function (res) {
+          console.log(res);
+        });
+    },
+  },
+};
+</script>
